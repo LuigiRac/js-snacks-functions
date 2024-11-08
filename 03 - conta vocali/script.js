58336fba53b96fb6fef0af0d9ea54597beba8e0c
LuigiRac <lui.rac06@gmail.com>
2024-11-08 21:36:32 +0100

    3_exercise

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function numVocali() {
+    const vocali = ["a", "e", "i", "o", "u"];
+    const onlyVocali = [];
 
+    for (let i = 0; i < word.length; i++) {
+        if (vocali.includes(word[i])) {
+            onlyVocali.push(word[i])
+        }
+
+    }
+
+    return onlyVocali
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(numVocali());
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
