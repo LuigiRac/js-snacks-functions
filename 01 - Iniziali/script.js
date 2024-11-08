af92d69effe4de3eca5cc876fdc89f9c04a984d6
LuigiRac <lui.rac06@gmail.com>
2024-11-08 21:06:09 +0100

    1 exercise

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function firstLetter() {
+    // console.log(names.length);
+    const newArray = [];
 
+    for (let i = 0; i < names.length; i++) {
+
+
+        let fLetter = names[i].charAt(0);
+        newArray.push(fLetter)
+
+        // console.log(fLetter);
+    }
+
+    return newArray;
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+// firstLetter()
+console.log(firstLetter());
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,7 +4,9 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function firstLetter(params) {
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
